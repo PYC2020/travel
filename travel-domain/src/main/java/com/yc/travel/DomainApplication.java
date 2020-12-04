635ca2b793903184272078f02cd506a935e16644
19537 <1953745128@qq.com>
2020-12-05 00:03:11 +0800

    product

--- travel-domain/src/main/java/com/yc/travel/DomainApplication.java
+++ travel-domain/src/main/java/com/yc/travel/DomainApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DomainApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(DomainApplication.class, args
         );
     }
