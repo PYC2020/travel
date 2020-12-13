ece55fed88e3fe017f4256bae84cf9427eb658e5
19537 <1953745128@qq.com>
2020-12-13 11:13:17 +0800

    1

--- travel-product/src/test/java/product/TestService.java
+++ travel-product/src/test/java/product/TestService.java
@@ -9,8 +9,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
 
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Logger;
 
 @RunWith(SpringRunner.class)
@@ -30,6 +32,23 @@ public class TestService {
         logger.info("调用Service.list");
         List<ProductDomain> list = productService.list();
         System.out.println(list);
+    }
+
+
+    @Test
+    public void testSave() {
+        Random r = new Random();
+        ProductDomain productDomain = new ProductDomain(16, "", 3,77,"",77,
+                "","");
+        productService.save(productDomain);
+        logger.info("新增的产品编号:" + productDomain.getPid());
+        //断言.
+        Assert.notNull(productDomain.getPid(), "not insert");
+    }
+
+    @Test
+    public void testDelete() {
+        productService.delete(16);
     }
 
 
--- travel-product/src/test/java/type/TestService.java
+++ travel-product/src/test/java/type/TestService.java
@@ -1,5 +1,6 @@
 package type;
 
+import com.yc.product.domain.ProductDomain;
 import com.yc.type.domain.TypeDomain;
 import com.yc.type.service.TypeService;
 import org.junit.Test;
@@ -7,8 +8,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
 
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Logger;
 
 @RunWith(SpringRunner.class)
@@ -25,6 +28,21 @@ public class TestService {
         logger.info("调用typeService.list");
         List<TypeDomain> list = typeService.list();
         System.out.println(list);
+    }
+
+    @Test
+    public void testSave() {
+        Random r = new Random();
+        TypeDomain typeDomain = new TypeDomain(16, "", "",77);
+        typeService.save(typeDomain);
+        logger.info("新增的产品编号:" + typeDomain.getTno());
+        //断言.
+        Assert.notNull(typeDomain.getTno(), "not insert");
+    }
+
+    @Test
+    public void testDelete() {
+        typeService.delete(16);
     }
 
 
